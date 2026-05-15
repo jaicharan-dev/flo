c9d56177f7082d98e95257b8d42fa70cf0f53584
jaicharan-dev <jaicharan.dev@gmail.com>
2026-05-15 19:30:18 +0530

    added login end point

--- flo-backend/main.py
+++ flo-backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from passlib.context import CryptContext
+import jwt
+from datetime import datetime, timedelta
 
 from models import User, SessionLocal
 from sqlalchemy.orm import Session
@@ -35,5 +37,30 @@ def register_user(user_data: UserCreate, db: Session = Depends(get_db)):
 
     return {"message": "User created successfully", "user_id": new_user.id}
 
+SECRET_KEY = "flo-super-secret-key-for-jwt"
+ALGORITHM = "HS256"
 
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+@app.post("/login")
+def login_user(user_data: UserLogin, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == user_data.email).first()
+    if not user or not pwd_context.verify(user_data.password, user.password_hash):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    expiration_time = datetime.utcnow() + timedelta(hours=2)
+
+    payload = {
+        "sub" : str(user.id),
+        "exp" : expiration_time
+    }
+
+    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+    return {
+        "access_token": token, 
+        "token_type": "bearer",
+        "message": "Login successful"
+    }
 
